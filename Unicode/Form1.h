cae20c39366f794eb826b61daa916eac3a698d3f
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-02-06 18:25:55 +0200

    Added old VS2008 project to mercurial.

---
+++ Unicode/Form1.h
@@ -0,0 +1,110 @@
+#pragma once
+
+
+namespace Unicode {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form1
+	///
+	/// WARNING: If you change the name of this class, you will need to change the
+	///          'Resource File Name' property for the managed resource compiler tool
+	///          associated with all .resx files this class depends on.  Otherwise,
+	///          the designers will not be able to interact properly with localized
+	///          resources associated with this form.
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	protected: 
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
+			this->SuspendLayout();
+			// 
+			// Form1
+			// 
+			resources->ApplyResources(this, L"$this");
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::Black;
+			this->DoubleBuffered = true;
+			this->ForeColor = System::Drawing::Color::Lime;
+			this->Name = L"Form1";
+			this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
+			this->ResizeBegin += gcnew System::EventHandler(this, &Form1::Form1_ResizeBegin);
+			this->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Form1::Form1_Paint);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void Form1_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
+				System::Drawing::Font^ drawFont = gcnew System::Drawing::Font( "Microsoft Sans Serif",7 );
+				SolidBrush^ drawBrush = gcnew SolidBrush( Color::Lime );
+				StringFormat^ drawFormat = gcnew StringFormat;
+
+				String^ drawString;
+				float x,y;
+				wchar_t c;
+				int count;
+
+
+				e->Graphics->Clear(Color::Black);
+				c = 0;
+				count = 0;
+				for( x = 20 ; x < Form1::Width ; x += 51 )
+					for( y = 5 ; y < Form1::Height - 50 ; y += 11 )					
+					{
+						drawString = "";
+						drawString += count++;
+						drawFormat->Alignment = StringAlignment::Far;
+						e->Graphics->DrawString(drawString,drawFont,drawBrush,x,y,drawFormat);
+						drawString = "";
+						drawString += c++;
+						drawFormat->Alignment = StringAlignment::Near;
+						e->Graphics->DrawString(drawString,drawFont,drawBrush,x+4,y,drawFormat);
+					}
+				e->Graphics->Flush();
+			 }
+	private: System::Void Form1_ResizeBegin(System::Object^  sender, System::EventArgs^  e) {
+			 }
+	};
+}
+
---
+++ Unicode/ReadMe.txt
@@ -0,0 +1,33 @@
+========================================================================
+    APPLICATION : Unicode Project Overview
+========================================================================
+
+AppWizard has created this Unicode Application for you.  
+
+This file contains a summary of what you will find in each of the files that
+make up your Unicode application.
+
+Unicode.vcproj
+    This is the main project file for VC++ projects generated using an Application Wizard. 
+    It contains information about the version of Visual C++ that generated the file, and 
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+Unicode.cpp
+    This is the main application source file.
+    Contains the code to display the form.
+
+Form1.h
+    Contains the implementation of your form class and InitializeComponent() function.
+
+AssemblyInfo.cpp
+    Contains custom attributes for modifying assembly metadata.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named Unicode.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
